80c0a0ef76c4f0e6372d85e1e4ba9db7e730ca28
Kosuke Kawagishi <kawagishi@tkrite.com>
2024-06-21 00:27:10 +0900

    web3d test

---
+++ sw.js
@@ -0,0 +1,48 @@
+const cacheName = "ddd_app";
+const cacheVersion = "1718896425883";
+const cacheFiles = ["ddd/AdaptiveToneMappingPass%40BQpMIpNU.js","ddd/BleachBypassShader%40oBD3ESB3.js","ddd/CSM%40yQX4v84e.js","ddd/ColladaLoader%40BkPnYAcc.js","ddd/DDSLoader%40C9wWxVpS.js","ddd/DRACOExporter%40m31_IZiN.js","ddd/DRACOLoader%40CxWNGOWb.js","ddd/DotScreenPass%40D6lz0-Gg.js","ddd/EXRLoader%407UhQUwQ2.js","ddd/ExtendedShaderMaterial%40CoOG-Exu.js","ddd/FBXLoader_139%40EwyvjmEk.js","ddd/FXAAShader%409iU-ryRt.js","ddd/FilmPass%40CCos2mq6.js","ddd/FontLoader%40CPDo5AhB.js","ddd/GLTFExporter%40cbBE5ysc.js","ddd/GLTFLoader%40CYrxnVa8.js","ddd/GTAOPass%40BZIQrJwt.js","ddd/GlitchPass%40XRdZ3JKT.js","ddd/HBAOPass%40DG1WO0kq.js","ddd/HTMLMesh%40Cw3vxJK3.js","ddd/HalftonePass%40DGNnSRGu.js","ddd/InteractiveGroup%40IhZzUAGC.js","ddd/LUT3dlLoader%40CtudW5s0.js","ddd/LUTCubeLoader%40DJaJYyAj.js","ddd/LUTPass%40Cis5h_qc.js","ddd/LWOLoader%40B-IkBQlA.js","ddd/MMDExporter%40BBH_vGcK.js","ddd/MTLLoader%40Bhbdx6IQ.js","ddd/MipGenerationShader%4034s1mtIh.js","ddd/MosaicPass%40C9gjqo_t.js","ddd/OBJExporter%40PqenuyH8.js","ddd/OBJLoader%40DdvHj1NM.js","ddd/OculusHandModel%40B87Z4B-8.js","ddd/OutlineEffect%40CWld7flV.js","ddd/OutlinePass%40B1OyCB37.js","ddd/PLYExporter%40U95Wcqui.js","ddd/PLYLoader%40Bcs7uwLY.js","ddd/PVRLoader%40CwxczKK9.js","ddd/PackedShader%40D9dUVuxe.js","ddd/ParticleEngine%40DmMsVNVO.js","ddd/ParticleEngineExamples%40BJYno18w.js","ddd/PositionalAudioHelper%40Dwlng3zX.js","ddd/ProgressiveLightMap%40CVbx9wrO.js","ddd/RGBMLoader%40Bbtewkqq.js","ddd/RectAreaLightHelper%40CSAnfQbR.js","ddd/Reflector%40Y262dkt1.js","ddd/ReflectorForSSRPass%40DYrsoXuG.js","ddd/SAOPass%40YMIOqZcl.js","ddd/SSAARenderPass%40CzwPtIot.js","ddd/SSRPass%40BrBfgE-U.js","ddd/SSRPass%40BsVtJJvQ.js","ddd/STLExporter%40CINTP0JI.js","ddd/STLLoader%40D0IhA1jF.js","ddd/SVGLoader%40D0iCujXh.js","ddd/ShaderReplacement%40CxvBdKt5.js","ddd/SimplexNoise%40DJLmKNIm.js","ddd/Sky%40B8oVSal4.js","ddd/TAARenderPass%40C2E6-JSV.js","ddd/TDSLoader%40hMk6mf8I.js","ddd/TechnicolorShader%40DQqnUeVc.js","ddd/TextGeometry%40C2AAkLzk.js","ddd/TextureUtils%40YzsTNqKi.js","ddd/UPNG%40DClqjSi1.js","ddd/USDZExporter%40C091FDda.js","ddd/USDZLoader%40C78fv819.js","ddd/VertexNormalsHelper%40D9ynRzjZ.js","ddd/WasmMediaEncoder.min%40BMzX7eYn.js","ddd/Water2%40BrDsMR-4.js","ddd/Water%40CYCtuCT1.js","ddd/WrappedShaderMaterial%40jJQ4lvlR.js","ddd/XRButton%40noNxbqF0.js","ddd/XRControllerModelFactory%40JHFsW_iR.js","ddd/ammo.wasm%40BN1wDPbS.js","ddd/avif%40CeTeIjm9.wasm","ddd/avif%40Na9ph3od.js","ddd/bootstrap-icons%40DSXWTQaD.woff2","ddd/bootstrap-icons%40DTeOS7dS.woff","ddd/ddd%40CX4D8d_4.css","ddd/ddd%40DwDq534T.js","ddd/fflate.module%40Do-qMP1X.js","ddd/file_browser%40BBP9F_ql.js","ddd/gif%40DJx2jrbY.js","ddd/i18n/en.json","ddd/i18n/ja.json","ddd/icon.dev%40Bwy0jAdt.png","ddd/jsframe.min%40D6SUlaS-.js","ddd/jsm/libs/ammo.wasm.wasm","ddd/jsm/libs/basis/basis_transcoder.js","ddd/jsm/libs/basis/basis_transcoder.wasm","ddd/jszip.min%40TY6z0PXt.js","ddd/local.io%40D8K0uS0N.js","ddd/manifest.png","ddd/manifest%40CKHnufNj.webmanifest","ddd/memfs.io%40Ca_VUbMH.js","ddd/meshopt_decoder.module%40CkuQsJa_.js","ddd/mipSampleFunctions%40CE2NLcig.js","ddd/mp4-muxer%40BBplf3BZ.js","ddd/pako%40aYzIuDlj.js","ddd/tga%40YHfKEhzZ.js","ddd/three-vrm.module%40-fSVt3d3.js","ddd/ui.exporters%40DyOYhrfI.js","ddd/webdav.io%40pZn3n59n.js","ddd/webm-muxer%40C1mJMcWQ.js","ddd/xr%40DRMfj2L-.js","ddd/textures/equirectangular/venice_sunset_1k.hdr","ddd/textures/hardwood2_diffuse.ktx2","ddd/textures/hardwood2_roughness.ktx2","ddd/textures/hardwood2_bump.ktx2","hkb2elky0fct638g3r2y22f9/1z4099crd79knnshj02nj0ir/%E3%83%8F%E3%83%83%E3%83%94%E3%83%BC%E3%82%B7%E3%83%B3%E3%82%BB%E3%82%B5%E3%82%A4%E3%82%B6%20%E3%82%AB%E3%83%A1%E3%83%A9.vmd","hkb2elky0fct638g3r2y22f9/fo5tcl8yl49xph9h2xnc1h1m/hardwood2_diffuse.ktx2","hkb2elky0fct638g3r2y22f9/fo5tcl8yl49xph9h2xnc1h1m/hardwood2_bump.ktx2","hkb2elky0fct638g3r2y22f9/fo5tcl8yl49xph9h2xnc1h1m/hardwood2_roughness.ktx2","hkb2elky0fct638g3r2y22f9/1wjho2byu6zfam2ox55pvhyw/happysyn.vmd","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E3%81%A4%E3%81%BF%E5%BC%8F%E3%83%9F%E3%82%AF%E3%81%95%E3%82%93v4.pmx","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E9%A0%AD.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E7%9E%B3.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E9%AB%AA.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E4%BD%93.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E6%9C%8D1.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E6%9C%8D2.png","hkb2elky0fct638g3r2y22f9/w73b0v5m1lxtfpwqry30cuzo/%E8%A1%A8%E6%83%85.png","hkb2elky0fct638g3r2y22f9/9hqb8a0tmzvzecdj24a3arcz/y2mate.com%20-%20%E3%83%8F%E3%83%83%E3%83%94%E3%83%BC%E3%82%B7%E3%83%B3%E3%82%BB%E3%82%B5%E3%82%A4%E3%82%B6%20%20%E5%B7%A1%E9%9F%B3%E3%83%AB%E3%82%AB%20GUMI.mp3","manifest.webmanifest","manifest.png","hkb2elky0fct638g3r2y22f9/index.ddd"];
+
+
+console.warn("sw", cacheName, cacheVersion);
+
+
+self.addEventListener('install', (event) => {
+    console.warn("sw:install");
+    event.waitUntil(
+        caches.open(`${cacheName}:${cacheVersion}`).then((cache) => {
+            console.warn("sw:install:", cacheVersion);
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheKeys) {
+            return Promise.all(
+                cacheKeys.filter(function(cacheKey) {
+                    const [_cacheName, _cacheVersion] = cacheKey.split(':');
+                    return _cacheName == cacheName && _cacheVersion != cacheVersion;
+                }).map(function(cacheKey) {
+                    return caches.delete(cacheKey);
+                })
+            );
+        })
+    );
+});
+
+
+console.warn("sw2", cacheVersion);
+self.addEventListener('fetch', function (event) {
+    const arr = cacheFiles.filter((f) => event.request.url.endsWith(f));
+    if (arr.length > 0) {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                // if (response) console.warn("Cache hit:", cacheVersion, event.request.url);
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        return fetch(event.request);
+    }
+});
